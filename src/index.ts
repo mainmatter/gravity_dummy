5c248b02518b91019f3ab666198182d1adf232fa
Oscar Dominguez <dominguez.celada@gmail.com>
2022-09-15 14:38:53 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import lodash from "lodash";
 import cowsay from "cowsay";
 
-cowsay.say({ text: "Hello world" });
-
 console.log(lodash.merge({ a: 1 }, { b: 2 }));
+
+cowsay.say({ text: "Hello world" });
