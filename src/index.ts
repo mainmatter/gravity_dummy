302c55db8428b947a0ba0be920e135a97a108029
Inês Soares Silva <ines_ngc@hotmail.com>
2022-10-31 13:06:28 +0000

    A new 'Action Required' example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
 import lodash from "lodash";
 
 console.log(lodash.merge({ a: 1 }, { b: 2 }));
+console.log("Let's add a lot of console.logs");
+
+console.log("Let's add a lot of console.logs");
+
+console.log("Let's add a lot of console.logs");
+
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
+console.log("Let's add a lot of console.logs");
