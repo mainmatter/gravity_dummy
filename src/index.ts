be050fc7e9663c02eaded57ff17eaf20ea93acd3
Inês Soares Silva <ines_ngc@hotmail.com>
2022-11-22 11:54:10 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,3 +30,7 @@ console.log(lodash.merge({ a: 1 }, { b: 2 }));
 console.log(lodash.merge({ a: 1 }, { b: 2 }));
 console.log(lodash.merge({ a: 1 }, { b: 2 }));
 console.log(lodash.merge({ a: 1 }, { b: 2 }));
+console.log(lodash.merge({ a: 1 }, { b: 2 }));
+console.log(lodash.merge({ a: 1 }, { b: 2 }));
+console.log(lodash.merge({ a: 1 }, { b: 2 }));
+console.log(lodash.merge({ a: 1 }, { b: 2 }));
